f6caac4a8dc9ec8a5a6e1e570321185d9eae6102
Vivek <vivektheintel@gmail.com>
2024-09-17 21:08:44 -0400

    Base push

---
+++ src/assignment4/Main.java
@@ -0,0 +1,11 @@
+package assignment4;
+
+/**
+ * @author Vivek
+ * @since 2024-09-17
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
